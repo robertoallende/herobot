bbd2e833ffcd3366bf1317b2e36c4db82e15c436
rallende <rallende@e8ae04f8-9d49-0410-aec4-f1edc0e25301>
2008-03-30 20:38:19 +0000

    Adding the class skeleton

---
+++ trunk/lib/board.py
@@ -0,0 +1,17 @@
+"""Game brackground
+"""
+import sys
+import getopt
+
+class Board:
+    def __init__(self, game, pos):
+        pass
+
+
+# metodo baston 
+def main():
+     
+
+if __name__ == "__main__":
+    main()
+
