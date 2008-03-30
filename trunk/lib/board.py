a9951f6068ca04458ee8f0280e0e43b9d759b927
rallende <rallende@e8ae04f8-9d49-0410-aec4-f1edc0e25301>
2008-03-30 20:44:47 +0000

    Updating bastones

--- trunk/lib/board.py
+++ trunk/lib/board.py
@@ -10,7 +10,17 @@ class Board:
 
 # metodo baston 
 def main():
-     
+    # ver http://www.sacredchao.net/~piman/writing/sprite-tutorial.shtml
+    import pygame
+    from pygame.locals import *
+
+    pygame.init()
+    boxes = []
+
+    screen = pygame.display.set_mode([800, 600])
+    pygame.display.update()
+    while pygame.event.poll().type != KEYDOWN: pygame.time.delay(10) 
+
 
 if __name__ == "__main__":
     main()
