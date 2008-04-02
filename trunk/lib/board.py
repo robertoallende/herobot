ee8ad76887206be9c17191b551253ddbcd2d7007
rallende <rallende@e8ae04f8-9d49-0410-aec4-f1edc0e25301>
2008-04-02 20:17:11 +0000

    Adding counters

--- trunk/lib/board.py
+++ trunk/lib/board.py
@@ -1,5 +1,11 @@
 """Game brackground
 """
+# board_height = 576 
+# railone = 30% de board_height
+# railtwo = 50% de board_height
+# railthree = 70% de board_height
+# street = 90% de board_height
+
 import sys
 import getopt
 import pygame
@@ -25,91 +31,122 @@ class Box(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.topleft = initial_position
 
+class Text(pygame.sprite.Sprite):
+    def __init__(self, text, initial_pos, center=0, color = (255, 255, 255)):
+        pygame.sprite.Sprite.__init__(self)
+        self.text = text
+        self.render_text = panel_font.render(text, True, color)
+        self.image = self.render_text
+
+        if center: 
+            pos = (initial_pos[0]-(self.render_text.get_width() / 2 ), initial_pos[1])
+        else:
+            pos = (initial_pos[0] , initial_pos[1])
+
+        self.rect = list(pos)
+        self.width = self.render_text.get_width()
+        self.height = self.render_text.get_height()
+        self.alive = True
+        self.time_passed = 0
+        self.initial_pos = pos
+        self.color = color
+
+    def __str__(self):
+        return self.text
+
+    def update(self, time_passed, text):
+        self.text = text
+        self.render_text = panel_font.render(text, True, self.color)
+        self.image = self.render_text
+        self.rect = list(self.initial_pos)
+        self.width = self.render_text.get_width()
 
 class Board:
 
-    def __init__(self, game, screen):
+    def __init__(self, screen, game=1):
         self.game = game
-#        self.board_complete = 0
-#        self.board_timeout = -1
-#        self.paused = 0
-#        self.launched = 1
         self.screen = screen
+        self.time_passed = 0
 
-        self.graph_bottom = 18
+        self.graph_bottom = 20
         self.graph_sides = 2
 
         self.score = 0
         self.time = 30
         self.lives = 3
+        self.target = 100
 
         #background
         self.background = pygame.Surface(screen.get_size())
         self.background = self.background.convert()
         self.background.fill((0, 0, 0))
 
-        self.draw_score()
-        self.draw_time()
-        self.draw_lives()
+        self.sprites = pygame.sprite.RenderUpdates()
 
-        self.draw_background()
-        self.draw_railone()
-        self.draw_railtwo()
-        self.draw_railthree()
-        self.draw_street()
+
+        self.sprites.add( self.draw_background() )
+        self.sprites.add( self.draw_time() )
+        self.sprites.add( self.draw_score() )
+        self.sprites.add( self.draw_target() )
+        self.sprites.add( self.draw_lives() )
+#       self.draw_background()
 
     def draw_background(self):
-        b = Box([255, 0, 0], [0, 0], [screen_width, screen_height *  30 / 100 ]) 
-        self.background.blit(b.image, b.rect)
-
-    def draw_railone(self):
-        b = Box([0, 255, 0], [0, board_height *  30 / 100], \
-             [screen_width, screen_height *  20 / 100  ]) 
-        self.background.blit(b.image, b.rect)
-
-    def draw_railtwo(self):
-        b = Box([0, 0, 255], [0, board_height *  50 / 100], \
-             [screen_width, screen_height *  20 / 100 ]) 
-        self.background.blit(b.image, b.rect)
-
-    def draw_railthree(self):
-        b = Box([255, 255, 255], [0, board_height *  70 / 100], \
-             [screen_width, screen_height *  20 / 100 ]) 
-        self.background.blit(b.image, b.rect)
-
-    def draw_street(self):
-        b = Box([0, 200, 200], [0, board_height *  90 / 100], \
-            [screen_width, screen_height *  10 / 100 ]) 
-        self.background.blit(b.image, b.rect)
+        b = Box([255, 0, 0], [0, 0], [screen_width, board_height ]) 
+        return(b)
 
     def draw_score(self):
-        #drawing score
-        score_text = panel_font.render( "Score: " + str(self.score) , 1, (255, 255, 255))
-        textpos = score_text.get_rect()
-        textpos.topleft = (self.graph_sides, screen_height - self.graph_bottom)
-        self.background.blit(score_text, textpos)
+        initial_pos = (self.graph_sides, screen_height - self.graph_bottom)
+        self.score_text = Text("Score: " + str(self.score) , initial_pos )
+        return self.score_text
 
     def draw_time(self):
-        #drawing time
-        time_text = panel_font.render( "Time Left: " + str(self.time) , 1, (255, 255, 255))
-        textpos = time_text.get_rect()
-        textpos.topleft = (self.background.get_rect().centerx - textpos.right /2 \
-                          , screen_height - self.graph_bottom)
-        self.background.blit(time_text, textpos)
+        initial_pos = (self.background.get_rect().centerx - 125 \
+                      , screen_height - self.graph_bottom)
+        self.time_text = Text("Time Left: " + str(self.time) , initial_pos, 1 )
+        return self.time_text
+
+
+    def draw_target(self):
+        initial_pos = (self.background.get_rect().centerx + 125 \
+                      , screen_height - self.graph_bottom)
+        self.target_text = Text("Level Target: " + str(self.time) , initial_pos, 1 )
+        return self.target_text
+
 
     def draw_lives(self):
         #lives 
-        time_text = panel_font.render( "Error Margin: " + str(self.lives) , 1, (255, 255, 255))
-        textpos = time_text.get_rect()
-        textpos.topleft = ( screen_width - textpos.right - self.graph_sides, screen_height -  \
-                          self.graph_bottom)
-        self.background.blit(time_text, textpos)
+        initial_pos = ( screen_width - 60 - self.graph_sides, screen_height -  \
+                          self.graph_bottom )
+        self.lives_text = Text("Error Margin: " + str(self.lives) , initial_pos, 1 )
+        return self.lives_text
 
-    def update(self):
+
+    def update(self, time_passed, hits ):
         '''Blit everything to the screen'''
-        self.screen.blit(self.background, (0, 0)) 
-        pygame.display.flip()
-        pygame.display.update()
+        if self.time_passed <= 0:
+            self.time -= 1
+            self.time_text.update(time_passed, "Time Left: " + str(self.time) )
+            self.time_passed = 1
+
+        if hits > 0:
+            self.score += hits * 10
+            self.score_text.update(time_passed, "Score: " + str( self.score ) )
+
+        if self.target > 0:
+            self.target -= hits
+            self.target_text.update(time_passed, "Level Target: " + str( self.target ) )
+
+        self.time_passed -= time_passed
+
+
+    def clear(self):
+        self.sprites.clear(self.screen, self.background)
+
+    def draw(self):
+        self.sprites.draw(self.screen)
+
+
 
 
 # metodo baston 
@@ -134,20 +171,22 @@ def main():
     load_fonts()
 
     pygame.init()
-    boxes = []
-
     screen = pygame.display.set_mode( [screen_width, screen_height] )
-    pygame.display.update()
 
-    board = Board(1, screen)
-    board.update()
+    board = Board(screen)
 
-    
-    while pygame.event.poll().type != KEYDOWN: pygame.time.delay(10) 
+    clock = pygame.time.Clock()
 
+    while( pygame.event.poll().type != KEYDOWN  ):
+        time_passed = clock.tick(30)
+        time_passed_seconds = time_passed / 1000.0
+        
+        board.update(time_passed_seconds, 1)
+
+        rectlist = board.draw()
+        pygame.display.update()
+
+        board.clear()
 
 if __name__ == "__main__":
     main()
-
-
-
