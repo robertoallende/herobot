1d23b4dc49ff65d0f3c495bb28419d2328d3297a
nueces <nueces@e8ae04f8-9d49-0410-aec4-f1edc0e25301>
2008-04-05 03:46:55 +0000

    *  intro_screen.py   retab

--- trunk/lib/intro_screen.py
+++ trunk/lib/intro_screen.py
@@ -11,22 +11,22 @@ from string import split, join
 SCREEN_SIZE = (800, 600)
 #Parte string en frases de tamanio size(aunque no siempre)
 def get_phrases(string, size):
-	words = split(string)
-	result = []
-	while words:
-		actual_size = 0
-		actual_word = []
-		while words and actual_size+len(words[0]) < size:
-			actual_size += len(words[0])+1
-			actual_word.append(words.pop(0))
-		actual_word = join(actual_word)
-		if words:
-			i = 1
-			while len(actual_word) != size:
-				actual_word = actual_word.replace(' '*i, ' '*(i+1),  size - len(actual_word))
-				i += 1
-		result.append(actual_word)
-	return result
+    words = split(string)
+    result = []
+    while words:
+        actual_size = 0
+        actual_word = []
+        while words and actual_size+len(words[0]) < size:
+            actual_size += len(words[0])+1
+            actual_word.append(words.pop(0))
+        actual_word = join(actual_word)
+        if words:
+            i = 1
+            while len(actual_word) != size:
+                actual_word = actual_word.replace(' '*i, ' '*(i+1),  size - len(actual_word))
+                i += 1
+        result.append(actual_word)
+    return result
 
 class Board:
     def __init__(self, game, pos):
@@ -35,83 +35,83 @@ class Board:
 #Esta clase implementa frases que van subiendo en la pantalla achicandose
 #a medida que avanza
 class Phrase(pygame.sprite.Sprite):
-	def __init__(self, text, font, color, initial_pos, speed):
-		pygame.sprite.Sprite.__init__(self)
-		self.text = text
-		self.speed = speed
-		self.render_text = font.render(text, True, color)
-		self.image = self.render_text
-		self.rect = list(initial_pos)
-		self.width = self.render_text.get_width()
-		self.height = self.render_text.get_height()
-		self.alive = True
-		
-	def __str__(self):
-		return self.text
-	
-	def update(self, time_passed):
-		self.rect[1] -= time_passed*self.speed
+    def __init__(self, text, font, color, initial_pos, speed):
+        pygame.sprite.Sprite.__init__(self)
+        self.text = text
+        self.speed = speed
+        self.render_text = font.render(text, True, color)
+        self.image = self.render_text
+        self.rect = list(initial_pos)
+        self.width = self.render_text.get_width()
+        self.height = self.render_text.get_height()
+        self.alive = True
+        
+    def __str__(self):
+        return self.text
+    
+    def update(self, time_passed):
+        self.rect[1] -= time_passed*self.speed
 
-		#esta en la pantalla asi que hay que dibujarlo
-		if 0 < self.rect[1]+self.height and self.rect[1] < SCREEN_SIZE[1]:
-			self.width *= 0.994
-			self.height *= 0.994
-			self.rect[0] = (SCREEN_SIZE[0] - self.width)/2
-			self.image = pygame.transform.scale(self.render_text, (int(self.width), int(self.height)))
-		elif self.rect[1]+self.height <= 0:
-			for grp in self.groups():
-	   			grp.remove(self)
+        #esta en la pantalla asi que hay que dibujarlo
+        if 0 < self.rect[1]+self.height and self.rect[1] < SCREEN_SIZE[1]:
+            self.width *= 0.994
+            self.height *= 0.994
+            self.rect[0] = (SCREEN_SIZE[0] - self.width)/2
+            self.image = pygame.transform.scale(self.render_text, (int(self.width), int(self.height)))
+        elif self.rect[1]+self.height <= 0:
+            for grp in self.groups():
+                grp.remove(self)
 
 #Deberia mostrar algo parecido a los titulos de star wars, pero falta tunearlo un poco con las velocidades
 #def presentation(text, font, screen, speed=0.05):
-	#phrases = [Phrase(p, font, (255, 200, 0), SCREEN_SIZE, speed) for p in get_phrases(text, 30)]
-	#last_phrase = pygame.sprite.GroupSingle()
-	#screen_phrases = pygame.sprite.RenderUpdates()
-	
-	#clock = pygame.time.Clock()
-	#last_phrase.add(phrases[0])
-	#screen_phrases.add(phrases.pop(0))
-	#while screen_phrases:
-		#for event in pygame.event.get():
-			#if event.type == KEYDOWN:
-				#return
-		#if last_phrase:
-			#s  = last_phrase.sprite
-			#if phrases and s.rect[1]+s.height < SCREEN_SIZE[1] - 2:
-				#last_phrase.add(phrases[0])
-				#screen_phrases.add(phrases.pop(0))
-		#screen.fill((0,0,0))
-		#time_passed = clock.tick(30)
-		#screen_phrases.update(time_passed)
-		#rectlist = screen_phrases.draw(screen)
-		#pygame.display.update(rectlist)
-	
-	#while pygame.event.wait().type != KEYDOWN:pass
+    #phrases = [Phrase(p, font, (255, 200, 0), SCREEN_SIZE, speed) for p in get_phrases(text, 30)]
+    #last_phrase = pygame.sprite.GroupSingle()
+    #screen_phrases = pygame.sprite.RenderUpdates()
+    
+    #clock = pygame.time.Clock()
+    #last_phrase.add(phrases[0])
+    #screen_phrases.add(phrases.pop(0))
+    #while screen_phrases:
+        #for event in pygame.event.get():
+            #if event.type == KEYDOWN:
+                #return
+        #if last_phrase:
+            #s  = last_phrase.sprite
+            #if phrases and s.rect[1]+s.height < SCREEN_SIZE[1] - 2:
+                #last_phrase.add(phrases[0])
+                #screen_phrases.add(phrases.pop(0))
+        #screen.fill((0,0,0))
+        #time_passed = clock.tick(30)
+        #screen_phrases.update(time_passed)
+        #rectlist = screen_phrases.draw(screen)
+        #pygame.display.update(rectlist)
+    
+    #while pygame.event.wait().type != KEYDOWN:pass
 
 def draw_text(phrases, font, screen, speed=0.05):
-	phrases = [Phrase(p, font, (255, 200, 0), SCREEN_SIZE, speed) for p in phrases]
-	last_phrase = pygame.sprite.GroupSingle()
-	screen_phrases = pygame.sprite.RenderUpdates()
-	
-	clock = pygame.time.Clock()
-	last_phrase.add(phrases[0])
-	screen_phrases.add(phrases.pop(0))
-	while screen_phrases:
-		for event in pygame.event.get():
-			if event.type == KEYDOWN:
-				return
-		if last_phrase:
-			s  = last_phrase.sprite
-			if phrases and s.rect[1]+s.height < SCREEN_SIZE[1] - 2:
-				last_phrase.add(phrases[0])
-				screen_phrases.add(phrases.pop(0))
-		screen.fill((0,0,0))
-		time_passed = clock.tick(30)
-		screen_phrases.update(time_passed)
-		rectlist = screen_phrases.draw(screen)
-		pygame.display.update(rectlist)
-	
-	while pygame.event.wait().type != KEYDOWN:pass
+    phrases = [Phrase(p, font, (255, 200, 0), SCREEN_SIZE, speed) for p in phrases]
+    last_phrase = pygame.sprite.GroupSingle()
+    screen_phrases = pygame.sprite.RenderUpdates()
+    
+    clock = pygame.time.Clock()
+    last_phrase.add(phrases[0])
+    screen_phrases.add(phrases.pop(0))
+    while screen_phrases:
+        for event in pygame.event.get():
+            if event.type == KEYDOWN:
+                return
+        if last_phrase:
+            s  = last_phrase.sprite
+            if phrases and s.rect[1]+s.height < SCREEN_SIZE[1] - 2:
+                last_phrase.add(phrases[0])
+                screen_phrases.add(phrases.pop(0))
+        screen.fill((0,0,0))
+        time_passed = clock.tick(30)
+        screen_phrases.update(time_passed)
+        rectlist = screen_phrases.draw(screen)
+        pygame.display.update(rectlist)
+    
+    while pygame.event.wait().type != KEYDOWN:pass
 
 
 def presentation(text, font, screen, speed=0.05):
@@ -130,16 +130,16 @@ phrase = 'Game Over.\n razon ninguna.'
 
 # metodo baston
 def main():
-	from pygame.locals import *
-	from data import filepath
+    from pygame.locals import *
+    from data import filepath
 
-	font_filename = "GROOT___.TTF" #"Carnevalee Freakshow.ttf" #"pointy.ttf"  #"GROOT___.TTF"
-	pygame.init()
-	#font = pygame.font.SysFont("arial", 100)
-	font = pygame.font.Font( filepath( font_filename ), 100)
-	screen = pygame.display.set_mode(SCREEN_SIZE)
-	text_end_level(3, 'end level', 200, font, screen)
-		
+    font_filename = "GROOT___.TTF" #"Carnevalee Freakshow.ttf" #"pointy.ttf"  #"GROOT___.TTF"
+    pygame.init()
+    #font = pygame.font.SysFont("arial", 100)
+    font = pygame.font.Font( filepath( font_filename ), 100)
+    screen = pygame.display.set_mode(SCREEN_SIZE)
+    text_end_level(3, 'end level', 200, font, screen)
+        
 if __name__ == "__main__":
-	main()
+    main()
 
